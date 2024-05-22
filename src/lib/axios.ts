67d6c48f4289a0da0c8f7ec619c308cf0f943be1
Daniel <danieldcc.colares@gmail.com>
2024-05-22 18:40:45 -0300

    loading user profile state

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,12 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
+
+// simulate web delay when working in development ambient
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
