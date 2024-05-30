e61d8d4d3802d5d3bdc161851ce091e793a4d389
Daniel <danieldcc.colares@gmail.com>
2024-05-30 10:54:36 -0300

    update simulate web delay in axios api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -10,7 +10,9 @@ export const api = axios.create({
 // simulate web delay when working in development ambient
 if (env.VITE_ENABLE_API_DELAY) {
   api.interceptors.request.use(async (config) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.round(Math.random() * 3000)),
+    )
 
     return config
   })
